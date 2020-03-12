161413712ce61514034b0dc03419c7eff5138dab
piperlewis <30031413+piperlewis@users.noreply.github.com>
2020-03-12 15:57:47 +0000

    Uploading all files

---
+++ stonehavenpipeband.github.io/js/script.js
@@ -0,0 +1,66 @@
+// JavaScript Document
+
+//Gallery page
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("column_grid");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("btnactive");
+    current[0].className = current[0].className.replace(" btnactive", "");
+    this.className += " btnactive";
+  });
+}
+
+
+//Thank you Notification in Contact Form
+ function submitClick() {
+    if (formValidation()) {
+      alert("Thank you for your time! Your details have been submitted!");
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function formValidation() {
+    flag = true;
+
+    return flag;
+  }
+
+
+
